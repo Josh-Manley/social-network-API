1c5cdc28005c17baef61a34bf22340088032a34b
Josh-Manley <joshmanley45@gmail.com>
2024-04-29 21:46:27 -0600

    added post thoughts route

--- controllers/api/thoughtRoutes.js
+++ controllers/api/thoughtRoutes.js
@@ -1,10 +1,49 @@
-const { Thought } = require('../../models');
+const { Thought, User } = require('../../models');
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
   try {
     const thoughtData = await Thought.find();
     res.json(thoughtData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:thoughtId', async (req, res) => {
+  try {
+    const thoughtData = await Thought.findOne({ _id: req.params.thoughtId });
+    if (!thoughtData) {
+      return res.status(404).json({ message: 'No though with that id' });
+    }
+    res.json(thoughtData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// POST to create a new thought and associate it with a user
+router.post('/', async (req, res) => {
+  try {
+    const { thoughtText, username, userId } = req.body;
+
+    // Create a new thought using the provided data
+    const newThought = await Thought.create({ thoughtText, username });
+
+    // Find the user by userId
+    const user = await User.findById(userId);
+    if (!user) {
+      // If user is not found, delete the newly created thought (rollback)
+      await newThought.remove();
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    // Add the newly created thought's _id to the user's thoughts array
+    user.thoughts.push(newThought._id);
+    await user.save();
+
+    // Return the newly created thought data in the response
+    res.json(newThought);
   } catch (err) {
     res.status(500).json(err);
   }
--- models/thoughts.js
+++ models/thoughts.js
@@ -1,11 +1,18 @@
 const { Schema, model, Types } = require('mongoose');
 
-const reactionSchema = new Schema({
-  reactionId: { type: Types.ObjectId, default: () => new Types.ObjectId() },
-  reactionBody: { type: String, required: true, maxlength: 280 },
-  username: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
-});
+const reactionSchema = new Schema(
+  {
+    reactionId: { type: Types.ObjectId, default: () => new Types.ObjectId() },
+    reactionBody: { type: String, required: true, maxlength: 280 },
+    username: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
 
 const thoughtSchema = new Schema(
   {
